83ac98396ab962d6d7783ed74c3304b8790e5d9c
AlicePetcu <alice.petcu93@gmail.com>
2024-02-13 10:30:59 +0200

    Sent email with titles and descriptions

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import requests
+from send_email import send_email
 
 api_key = "b6c84b9bd7f94625a3786f3edbf17de4"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-01-09&sortBy" \
-      "=publishedAt&apiKey=b6c84b9bd7f94625a3786f3edbf17de4"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-01-12&sortBy=publishedAt&apiKey=b6c84b9bd7f94625a3786f3edbf17de4"
 
 # Make request
 request = requests.get(url, verify=False)
 
 # Get a dictionary with data
 content = request.json()
-
-# Access article titles and descriptions
+message = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    print(type(article["title"]), type(article["description"]))
+    message = message + article["title"] + "\n" + article["description"] + 2*"\n"
+
+message = message.encode("utf-8")
+send_email(message)
+
